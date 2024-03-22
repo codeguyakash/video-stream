cba9b08e362c1109ada397223003b7ab5c275b6c
Akash (codeguyakash) <codeguyakash@gmail.com>
2024-03-23 00:28:31 +0530

    intialize express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("first");
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+app.get("/", (req, res) => {
+  res.send("okk");
+});
+
+app.listen(PORT, () => {
+  console.log("Server Running...", PORT);
+});
